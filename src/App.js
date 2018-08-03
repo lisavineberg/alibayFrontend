99f38a3a8c12bd9fb580f84c44a66987ed202903
Lisa Vineberg <lisavineberg@Lisas-MacBook-Air.local>
2018-08-03 14:38:46 -0400

    modifications to createAListing

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
       <BrowserRouter>
       <div className="App">
        Hi guys!
-       <Route path='/BuyHomepage' exact='false' render={ () => <BuyHomepage /> } />
-       <Route path='/CreateAListing' exact='true' render={ () => <CreateAListing /> } />
-       <Route path='/Login' exact='true' render={ () => <Login /> } />
+       <Route path='/BuyHomepage' exact={false} render={ () => <BuyHomepage /> } />
+       <Route path='/CreateAListing' exact={false} render={ () => <CreateAListing /> } />
+       <Route path='/Login' exact={true} render={ () => <Login /> } />
 
       </div>
       </BrowserRouter>
