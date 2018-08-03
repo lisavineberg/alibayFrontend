c5afd88872e4a009d7fb6b695f58041c131ed902
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-03 16:31:47 -0400

    homepage path  added to app.js

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import { Link } from 'react-router-dom';
+
+class Homepage extends Component{
+    constructor() {
+        super();
+        
+    }
+
+}
+
+export default Homepage;
+
+
