1058a80dc951bed0026c1c7a2c578076df15fdb8
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-03 17:14:10 -0400

    homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,24 @@ import { Link } from 'react-router-dom';
 class Homepage extends Component{
     constructor() {
         super();
-        
+        this.state = {
+            username: ""
+        }
+    }
+    render(){
+        return(
+            <div>
+            <Link to="/MyHistory">
+                <button>My history</button>
+            </Link>  
+            <Link to="/DisplayItems">
+                <button>Buy Something</button>
+            </Link>
+            <Link to="/CreateAListing">
+                <button>Sell Something</button>
+            </Link>
+            </div>
+        )
     }
 
 }
