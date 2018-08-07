e572fbb1ff47983ca017345c886b462997496113
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-06 20:03:23 -0400

    item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
+   constuctor(){
+       super();
+       this.state = {}
+   }
+    componentDidMount(){
+    fetch('/getItem')
+        .then(response => response.text())
+        .then(responseBody => {
+            let itemdetails = JSON.parse(responseBody)
+            console.log(itemdetails)
+            this.setState({itemdetails})
+
+    })
+}   
     render(){
-        return (<div> Hi </div>)
+        return (
+            <div>
+            <div>Name:{this.state.item.itemName} </div>
+            <div>Description:{this.state.item.description} </div>
+            <div>Price:{this.state.item.description} </div>
+            <div>Seller Id:{this.state.item.sellerID} </div> 
+            </div>
+        
+        
+        
+        )
+
+       
     }
+    
 }
 
 export default Item
