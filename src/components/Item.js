df2ecb1b5bc310ede4b20db601b1e8a3c202097e
Shaun Messenger <shaunmessenger@Shauns-MacBook-Air.local>
2018-08-07 12:01:21 -0400

    item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class Item extends Component {
-   constuctor(){
+   constructor(){
        super();
        this.state = {}
    }
+   // make a fetch before the page renders, grab item info from backend
     componentDidMount(){
+        // it takes a small amount of time to run a fetch
     fetch('/getItem')
         .then(response => response.text())
         .then(responseBody => {
@@ -15,13 +18,25 @@ class Item extends Component {
 
     })
 }   
+//use a function to create a stateless component
+displayitem = () => {
+    return (
+        <div>
+            <ul>
+                <li>Name: {this.state.itemdetails.itemName} </li>
+                <li>Description: {this.state.itemdetails.description} </li>
+                <li>Price: {this.state.itemdetails.price} </li>
+                <li>Seller Id: {this.state.itemdetails.sellerId} </li>
+            </ul>
+        </div>
+    )
+}
+
     render(){
         return (
             <div>
-            <div>Name:{this.state.item.itemName} </div>
-            <div>Description:{this.state.item.description} </div>
-            <div>Price:{this.state.item.description} </div>
-            <div>Seller Id:{this.state.item.sellerID} </div> 
+             {this.state.itemdetails?this.displayitem():null}
+             <Link to='/Homepage'>Link to homepage</Link>
             </div>
         
         
