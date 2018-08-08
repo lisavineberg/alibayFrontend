77bfcb017de5cc0b6637e121b8f04372b149cde3
Lisa Vineberg <lisavineberg@Lisas-MacBook-Air.local>
2018-08-08 11:19:40 -0400

    changed homescreen

--- src/components/NavbarHomepage.js
+++ src/components/NavbarHomepage.js
@@ -14,7 +14,7 @@ class NavbarHomepage extends Component {
         return (
             <div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#" style={logostyle}>Navbar</a>
+  <a class="navbar-brand" href="#" style={logostyle}><Link to='/Homepage'>Keke</Link></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
